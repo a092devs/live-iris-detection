6942762fd9c4f9a3d6de0821ad29b7977697689e
Arsalan <35004378+a092devs@users.noreply.github.com>
2023-09-20 19:26:40 +0530

    added support for multiple people
    earlier it used to detect only one person.
    I have added 10 faces by default. 
    Larger number will result in laggy performance.

--- iris_detection.py
+++ iris_detection.py
@@ -5,16 +5,15 @@ import mediapipe as mp
 cap = cv.VideoCapture(0)
 mp_face_mesh = mp.solutions.face_mesh
 
-# Left iris indices
-LEFT_IRIS = [474, 475, 476, 477]
-# Right iris indices
-RIGHT_IRIS = [469, 470, 471, 472]
+# Iris indices
+LEFT_IRIS = list(range(474, 478))  # Indices for the left iris
+RIGHT_IRIS = list(range(469, 473))  # Indices for the right iris
 
 with mp_face_mesh.FaceMesh(
-    max_num_faces=1,
-    refine_landmarks=True, # Important for iris detection, as it has 478 landmarks instead of 468
-    min_detection_confidence=0.5, 
-    min_tracking_confidence=0.5    
+    max_num_faces=10,  # Detect 10 faces, can be adjusted. Larger number degrades performance
+    refine_landmarks=True,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
 ) as face_mesh:
     while True:
         ret, frame = cap.read()
@@ -35,21 +34,22 @@ with mp_face_mesh.FaceMesh(
 
         # Extract landmarks for Iris Detection
         if results.multi_face_landmarks:
-            mesh_points = np.array([np.multiply([p.x, p.y], [width, height]).astype(int) for p in results.multi_face_landmarks[0].landmark]) # detects 478 landmarks
+            for landmarks in results.multi_face_landmarks:
+                mesh_points = np.array([np.multiply([p.x, p.y], [width, height]).astype(int) for p in landmarks.landmark])
 
-            # Get center and radius of the Iris
-            # l_cx = left iris center x, l_cy = left iris center y, l_radius = left iris radius
-            # r_cx = right iris center x, r_cy = right iris center y, r_radius = right iris radius
-            (l_cx, l_cy), l_radius = cv.minEnclosingCircle(mesh_points[LEFT_IRIS])
-            (r_cx, r_cy), r_radius = cv.minEnclosingCircle(mesh_points[RIGHT_IRIS])
+                # Get center and radius of the left and right irises
+                (l_cx, l_cy), l_radius = cv.minEnclosingCircle(mesh_points[LEFT_IRIS])
+                (r_cx, r_cy), r_radius = cv.minEnclosingCircle(mesh_points[RIGHT_IRIS])
 
-            # Convert all values to int
-            center_left = np.array([int(l_cx), int(l_cy)], dtype=np.int32)
-            center_right = np.array([int(r_cx), int(r_cy)], dtype=np.int32)
+                # Convert all values to int
+                l_center = np.array([int(l_cx), int(l_cy)], dtype=np.int32)
+                r_center = np.array([int(r_cx), int(r_cy)], dtype=np.int32)
 
-            # Draw circles on Iris
-            cv.circle(frame, center_left, int(l_radius), (0, 255, 0), 1, cv.LINE_AA)
-            cv.circle(frame, center_right, int(r_radius), (0, 255, 0), 1, cv.LINE_AA)
+                # Draw circles on Irises
+                if l_radius > 0:
+                    cv.circle(frame, l_center, int(l_radius), (0, 255, 0), 1, cv.LINE_AA)
+                if r_radius > 0:
+                    cv.circle(frame, r_center, int(r_radius), (0, 255, 0), 1, cv.LINE_AA)
 
         image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         cv.imshow('Frame', frame)
