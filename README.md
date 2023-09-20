db96bc2026a7ee3d449ea49294629b5aca9664c6
Arsalan <35004378+a092devs@users.noreply.github.com>
2023-09-20 12:14:30 +0530

    added usage instruction

---
+++ README.md
@@ -0,0 +1,20 @@
+# Live Iris Detection
+
+## Usage
+
+### 1. Clone the repository
+```bash
+git clone https://github.com/a092devs/live-iris-detection
+```
+
+### 2. Install the requirements
+```bash
+pip install -r requirements.txt
+```
+
+### 3. Run the script
+```bash
+python iris_detection.py
+```
+
+## That's it!
--- iris_detection.py
+++ iris_detection.py
@@ -12,7 +12,7 @@ RIGHT_IRIS = [469, 470, 471, 472]
 
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,
-    refine_landmarks=True, # important for iris detection, as it has 478 landmarks instead of 468
+    refine_landmarks=True, # Important for iris detection, as it has 478 landmarks instead of 468
     min_detection_confidence=0.5, 
     min_tracking_confidence=0.5    
 ) as face_mesh:
@@ -33,21 +33,11 @@ with mp_face_mesh.FaceMesh(
         # Process the image and find faces
         results = face_mesh.process(rgb)
 
-        # Extract landmarks
+        # Extract landmarks for Iris Detection
         if results.multi_face_landmarks:
-            # print(results.multi_face_landmarks[0].landmark)
             mesh_points = np.array([np.multiply([p.x, p.y], [width, height]).astype(int) for p in results.multi_face_landmarks[0].landmark]) # detects 478 landmarks
-            # print(mesh_points.shape)
 
-            # Draw lines on the boundary of eyelids
-            # cv.polylines(frame, [mesh_points[LEFT_EYE]], True, (0, 255, 0), 1, cv.LINE_AA)
-            # cv.polylines(frame, [mesh_points[RIGHT_EYE]], True, (0, 255, 0), 1, cv.LINE_AA)
-
-            # Draw squares on iris
-            # cv.polylines(frame, [mesh_points[LEFT_IRIS]], True, (0, 255, 0), 1, cv.LINE_AA)
-            # cv.polylines(frame, [mesh_points[RIGHT_IRIS]], True, (0, 255, 0), 1, cv.LINE_AA)
-
-            # Get center and radius of the iris
+            # Get center and radius of the Iris
             # l_cx = left iris center x, l_cy = left iris center y, l_radius = left iris radius
             # r_cx = right iris center x, r_cy = right iris center y, r_radius = right iris radius
             (l_cx, l_cy), l_radius = cv.minEnclosingCircle(mesh_points[LEFT_IRIS])
@@ -57,7 +47,7 @@ with mp_face_mesh.FaceMesh(
             center_left = np.array([int(l_cx), int(l_cy)], dtype=np.int32)
             center_right = np.array([int(r_cx), int(r_cy)], dtype=np.int32)
 
-            # Draw circles on iris
+            # Draw circles on Iris
             cv.circle(frame, center_left, int(l_radius), (0, 255, 0), 1, cv.LINE_AA)
             cv.circle(frame, center_right, int(r_radius), (0, 255, 0), 1, cv.LINE_AA)
 
